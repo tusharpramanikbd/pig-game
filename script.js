5382747bc02f4ca1341612084f16734dd1ac8d07
Tushar Pramanik <tushar.pramanik@lastingdynamics.com>
2024-01-15 13:21:59 +0600

    implemented initial game state

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Selecting elements
+const score0Elm = document.querySelector('#score--0');
+const score1Elm = document.querySelector('#score--1');
+const diseElm = document.querySelector('.dice');
+
+const initGameState = () => {
+  score0Elm.textContent = 0;
+  score1Elm.textContent = 0;
+
+  diseElm.classList.add('hidden');
+};
+
+initGameState();
