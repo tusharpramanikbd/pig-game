5170d2b4809e6128ce0ac601897e453bd6332d6e
Tushar Pramanik <tushar.pramanik@lastingdynamics.com>
2024-01-15 17:28:44 +0600

    implemented roll btn click logic

--- script.js
+++ script.js
@@ -3,13 +3,38 @@
 // Selecting elements
 const score0Elm = document.querySelector('#score--0');
 const score1Elm = document.querySelector('#score--1');
-const diseElm = document.querySelector('.dice');
+
+const diceElm = document.querySelector('.dice');
+
+const current0Elm = document.getElementById('current--0');
+const current1Elm = document.getElementById('current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
 
 const initGameState = () => {
   score0Elm.textContent = 0;
   score1Elm.textContent = 0;
 
-  diseElm.classList.add('hidden');
+  diceElm.classList.add('hidden');
+};
+
+const handleBtnRoll = () => {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  diceElm.classList.remove('hidden');
+  diceElm.src = `dice-${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    current0Elm.textContent = currentScore;
+  } else {
+    // TODO: update active player
+  }
 };
 
 initGameState();
+btnRoll.addEventListener('click', handleBtnRoll);
